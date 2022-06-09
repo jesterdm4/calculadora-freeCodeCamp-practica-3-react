826948ca1d5c709cd923eab348ebbb7b4241b766
Jose <lunaxp5@gmail.com>
2022-06-09 08:37:53 -0400

    parada por preguntas, pero progresando

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from "./imagenes/logo.png";
+import Boton from './componentes/Boton.jsx';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='logo-contenedor'>
+        <img className='logo' 
+          src={logo} 
+          alt='logo' 
+        />
+      </div>
+      <div className='contenedor-calculadora'>
+        <div className='fila'>
+          <Boton>
+            7
+          </Boton>
+        </div>
+        <div className='fila'></div>
+        <div className='fila'></div>
+        <div className='fila'></div>
+        <div className='fila'></div>
+      </div>
     </div>
   );
 }
---
+++ src/componentes/Boton.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+//aqui esta el codigo al que se le hace refrencia en la pregunta numero 7
+const Boton = (props) => {
+
+    const esOperador = valor => {
+        return isNaN(valor) && valor !== '.' && valor !== '=';
+    };
+
+    return (
+        <div className={`boton-contenedor ${esOperador(props.children) ? "operador" : null}`} >
+            {props.children}
+        </div>
+    );
+}
+
+export default Boton;
